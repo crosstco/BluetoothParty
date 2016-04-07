2dd73d3b070b5ddb89043ad1cd9965a893972cb9
John Gadbois <johnnygadbois8@gmail.com>
2016-04-07 10:47:55 -0500

    Basic set-up of Game Settings View Controller and Lobby List View Controller

--- Bluetooth Party/LobbyListViewController.swift
+++ Bluetooth Party/LobbyListViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class LobbyListViewController: UIViewController {
+class LobbyListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    @IBOutlet weak var myTableView: UITableView!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 1 //: replace 1 with number of lobbies
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let myCell = myTableView.dequeueReusableCellWithIdentifier("lobbyCell", forIndexPath: indexPath)
+        myCell.textLabel?.text = "Lobby Title"
+        myCell.detailTextLabel?.text = "Game Type"
+        return myCell
+    }
+
 
 }
