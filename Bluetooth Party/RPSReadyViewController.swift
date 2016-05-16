7c3bc9333340a95ddcd3c8f7dc19b04d5d4751ab
Colin <Colin@administrators-macbook-air.local>
2016-05-16 10:23:51 -0500

    Redid RPS UI

---
+++ Bluetooth Party/RPSReadyViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RPSReadyViewController.swift
+//  Bluetooth Party
+//
+//  Created by Colin on 5/16/16.
+//  Copyright © 2016 Bluetooth Party Development Team. All rights reserved.
+//
+
+import UIKit
+
+class RPSReadyViewController: UIViewController {
+    
+    
+    var
+    
+    
+    var seconds = 3
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        seconds = 3
+    }
+
+}
